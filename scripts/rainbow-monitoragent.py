ed6831e704cfbad4c5e86a82c59fd4911aeb2643
rainbowbreeze <381940+rainbowbreeze@users.noreply.github.com>
2022-12-28 02:04:50 +0100

    Fixed errors in the monitor agent

--- scripts/rainbow-monitoragent.py
+++ scripts/rainbow-monitoragent.py
@@ -1,12 +1,13 @@
 # Start a service that returns some core machine signal, in a plan JSON format
 #
-# curl localhost:23123
+# curl --get localhost:23123
 #
 # Part of the RainbowScripts suite
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import datetime
+import json
 
 hostName = "0.0.0.0"
 serverPort = 23123
@@ -17,10 +18,18 @@ class MyServer(BaseHTTPRequestHandler):
         with open(r"/sys/class/thermal/thermal_zone0/temp") as File:
             _cpu_temp = File.readline()
         
+        # Creates the final signal object
+        _signals = {
+            "cpu_temp": int(_cpu_temp) / 1000
+        }
+        # and return the string
+        _signals_str = json.dumps(_signals)
+
+
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
-        self.wfile.write(bytes("{\"cpu_temp\":" + _cpu_temp / 1000 + "}", "utf-8"))
+        self.wfile.write(bytes(_signals_str, "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
